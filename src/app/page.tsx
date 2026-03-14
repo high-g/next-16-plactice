20061954bab9232d1efcc7d3c119215cbe44e8ae
fumiya <altema1217bbfumi@live.jp>
2026-03-14 19:32:20 +0900

    RSCのgetTime関数をコメントアウトし、SSRのgetTimeSSR関数を使用して時間を取得するように変更

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,23 @@
 import Counter from '@/components/Counter'
 
+// RSC
 async function getTime() {
-  return new Date().toISOString()
+  // return new Date().toISOString()
+}
+
+// SSR
+async function getTimeSSR() {
+  const res = await fetch('http://jsonplaceholder.typicode.com/posts/1', { cache: 'no-store' })
+  return res.json()
 }
 
 export default async function Home() {
-  const time = await getTime()
+  // RSC
+  // const time = await getTime()
+
+  // SSR
+  const res = await getTimeSSR()
+  const time = res.body
 
   return (
     <main>
