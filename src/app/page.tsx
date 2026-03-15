7b9bfb330ae85ed87dbe055269d49d92b98f7157
fumiya <altema1217bbfumi@live.jp>
2026-03-15 17:57:11 +0900

    SSRとSSG、ISRの関数を追加し、時間表示の取得方法を修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,28 +2,50 @@ import Counter from '@/components/Counter'
 
 // RSC
 async function getTime() {
-  // return new Date().toISOString()
+  return { body: new Date().toISOString() }
 }
 
 // SSR
-async function getTimeSSR() {
+async function getPostSSR() {
+  // キャッシュしない → リクエストのたびにサーバで実行
   const res = await fetch('http://jsonplaceholder.typicode.com/posts/1', { cache: 'no-store' })
+  return res.json()
+}
+
+// SSG
+async function getPostSSG() {
+  // ビルド時に1回だけ取得してキャッシュ
+  const res = await fetch('http://jsonplaceholder.typicode.com/posts/1', { cache: 'force-cache' })
+  return res.json()
+}
+
+// ISR（一定時間ごとに再生成）
+async function getPostISR() {
+  // 60秒ごとに再取得
+  const res = await fetch('http://jsonplaceholder.typicode.com/posts/1', { next: { revalidate: 60 } })
   return res.json()
 }
 
 export default async function Home() {
   // RSC
-  // const time = await getTime()
+  // const res = await getTime()
 
   // SSR
-  const res = await getTimeSSR()
-  const time = res.body
+  // const res = await getPostSSR()
+
+  // SSG
+  // const res = await getPostSSG()
+
+  // ISR
+  const res = await getPostISR()
+
+  const body = res.body
 
   return (
     <main>
       {/*　Server Component のブロック */}
       <section>
-        <p>{time}</p>
+        <p>{body}</p>
       </section>
 
       {/* Client Component のブロック */}
