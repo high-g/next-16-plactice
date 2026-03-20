71e192209b2d662a804ca59334ce1bdb74a693bf
fumiya <altema1217bbfumi@live.jp>
2026-03-20 14:25:00 +0900

    Counterコンポーネントに環境変数の使用例を追加

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -9,6 +9,10 @@ export default function Counter() {
     <div>
       <p>{count}</p>
       <button onClick={() => setCount((c) => c + 1)}>カウントアップ</button>
+
+      {/* 環境変数の使用例 */}
+      {/* <p>{process.env.SECRET_KEY || ''}</p> */}
+      <p>{process.env.NEXT_PUBLIC_APP_NAME || ''}</p>
     </div>
   )
 }
