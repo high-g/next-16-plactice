5bdea4057c95d32beb31072b7473464118e15a5c
fumiya <altema1217bbfumi@live.jp>
2026-03-21 20:42:16 +0900

    fix: fetchのURLポート番号を修正

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -4,7 +4,7 @@ export async function submitMessage(formData: FormData) {
   const message = formData.get('message') as string
   console.log('サーバで受け取ったメッセージ：', message)
 
-  const res = await fetch('http://localhost:30001/posts', {
+  const res = await fetch('http://localhost:3001/posts', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ title: message }),
