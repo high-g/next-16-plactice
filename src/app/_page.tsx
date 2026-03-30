87028c2d2d057004285e8cbf6b0a552f2d4473e5
fumiya <altema1217bbfumi@live.jp>
2026-03-30 22:40:04 +0900

    fix: _pageを作成

---
+++ src/app/_page.tsx
@@ -0,0 +1,95 @@
+import Counter from '@/components/Counter'
+import { submitMessage } from './actions'
+
+import { client } from '@/lib/client'
+
+// RSC
+async function getTime() {
+  return { body: new Date().toISOString() }
+}
+
+// SSR
+async function getPostSSR() {
+  // キャッシュしない → リクエストのたびにサーバで実行
+  const res = await fetch('http://jsonplaceholder.typicode.com/posts/1', { cache: 'no-store' })
+  return res.json()
+}
+
+// SSG
+async function getPostSSG() {
+  // ビルド時に1回だけ取得してキャッシュ
+  const res = await fetch('http://jsonplaceholder.typicode.com/posts/1', { cache: 'force-cache' })
+  return res.json()
+}
+
+// ISR（一定時間ごとに再生成）
+async function getPostISR() {
+  // 60秒ごとに再取得
+  const res = await fetch('http://jsonplaceholder.typicode.com/posts/1', { next: { revalidate: 60 } })
+  return res.json()
+}
+
+// Hono RPC
+async function getPostsFromHonoRPC() {
+  const res = await client.posts.$get({}, { init: { cache: 'no-store' } })
+  return res.json()
+}
+
+async function getPostsFromHono() {
+  const res = await fetch('http://localhost:3001/posts', { cache: 'no-store' })
+  console.log('Honoからのレスポンス:', res)
+  return res.json()
+}
+
+export default async function Home() {
+  // RSC
+  // const res = await getTime()
+
+  // SSR
+  // const res = await getPostSSR()
+
+  // SSG
+  // const res = await getPostSSG()
+
+  // ISR
+  const res = await getPostISR()
+
+  // Honoからデータ取得
+  //const posts = await getPostsFromHono()
+
+  // Hono RPC
+  const posts = await getPostsFromHonoRPC()
+
+  const body = res.body
+
+  return (
+    <main>
+      {/* Server Component のブロック */}
+      <section>
+        <p>{body}</p>
+      </section>
+
+      {/* Client Component のブロック */}
+      <Counter />
+
+      {/* Server Action */}
+      <form action={submitMessage}>
+        <input name="message" type="text" placeholder="メッセージを入力" />
+        <button type="submit">送信</button>
+      </form>
+
+      <hr />
+
+      <table>
+        <tbody>
+          {posts.map((post: { id: number; title: string }) => (
+            <tr key={post.id}>
+              <td>{post.id}</td>
+              <td>{post.title}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </main>
+  )
+}
