7467884c9d38e6f0bef1080c9d3fc046d50f46ed
fumiya <altema1217bbfumi@live.jp>
2026-04-01 19:04:27 +0900

    chore: monorepo

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,4 +1,4 @@
 import { hc } from 'hono/client'
-import type { AppType } from '../../../hono-api/src/index'
+import type { AppType } from 'hono-api/src/index'
 
 export const client = hc<AppType>('http://localhost:3001')
